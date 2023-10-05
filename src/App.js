f13059fdeefe25ca84da77137d033b37546d6072
Jonas Andrade Silva <devzut@gmail.com>
2023-10-05 04:12:37 -0300

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Offer from "./Pages/Offer";
+import Profile from "./Pages/Profile";
+import ForgotPass from "./Pages/ForgotPass";
+import SingIn from "./Pages/SingIn";
+import SingUp from "./Pages/SingUp";
+import Home from "./Pages/Home";
+
 function App() {
   return (
-    <>
-      <p>Helo Wolrd!</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-Pass" element={<ForgotPass />} />
+        <Route path="/sing-in" element={<SingIn />} />
+        <Route path="/sing-up" element={<SingUp />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/forgotPass.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ForgtPass = () => {
+  return <div>ForgtPass</div>;
+};
+
+export default ForgtPass;
